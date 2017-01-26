d698a9471904216ad250e8b8f04931b258e53f5a
ACaller <avivacaller@gmail.com>
2017-01-26 22:34:06 +0000

    Initial Layout
    Initial template for the higher-lower number game after original was
    unfortunately deleted

---
+++ main/java/com/example/aviva/higherorlowergame/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.aviva.higherorlowergame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int num1 = 0;
+    int num2 = 1;
+    int score = 0;
+
+    private void Button1Click(){
+
+    }
+
+    private void Button2Click(){
+
+    }
+
+    private void checkNumbers(int a, int b) {
+
+    }
+
+    private void roll(){
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
